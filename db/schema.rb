0d083eeefa0d9adea89a24d1d831b423e42fbc91
Glauton Vieira <glauton.vieira@gmail.com>
2017-02-17 18:49:50 -0200

    Creating uploaders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217202731) do
+ActiveRecord::Schema.define(version: 20170217204616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170217202731) do
     t.text     "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
+    t.json     "documents"
   end
 
 end
